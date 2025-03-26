340c81e2521dfb569841825e1ce16df420079d7f
test <test@test.com>
2025-03-27 01:53:39 +0900

    Admin_branch: A + B 병합 및 DT 하이퍼파라미터 수정

--- 코드_공유.py
+++ 코드_공유.py
@@ -29,8 +29,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # 하이퍼파라미터 설정
 param_grid = {
     'criterion': ['gini', 'entropy'],
-    'max_depth': [2, 5],
-    'min_samples_split': [2, 10],
+    'max_depth': [2, 3, 4, 5],              # 🔄 수정
+    'min_samples_split': [2, 5, 10],        # 🔄 수정
     'min_samples_leaf': [1, 2, 4]
 }
 
